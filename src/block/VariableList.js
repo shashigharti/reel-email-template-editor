81e0cbe13be5b2aa4affc1dee48315da8efe0fd8
Shashi Gharti <shashi.gharti@gmail.com>
2025-07-30 18:09:27 +0545

    add feature to copy variable

--- src/block/VariableList.js
+++ src/block/VariableList.js
@@ -5,6 +5,7 @@ import HelpText from "./components/HelpText.js";
 
 export default function VariableList({ onInsert }) {
   const [variables, setVariables] = useState([]);
+  const [copied, setCopied] = useState(false);
 
   useEffect(() => {
     fetchVariables()
@@ -14,17 +15,19 @@ export default function VariableList({ onInsert }) {
       });
   }, []);
 
-  const handleCopy = () => {
-    const textToCopy = `{{${v.key}}}`;
-    navigator.clipboard
-      .writeText(textToCopy)
-      .then(() => {
-        alert(`Copied to clipboard: ${textToCopy}`);
-      })
-      .catch((err) => {
-        alert("Failed to copy text");
-        console.error("Copy failed:", err);
-      });
+  const handleCopy = async (text) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      setCopied(true);
+
+      console.log("text", text);
+
+      setTimeout(() => {
+        setCopied(false);
+      }, 2000);
+    } catch (err) {
+      console.error("Copy failed", err);
+    }
   };
 
   return (
@@ -33,7 +36,7 @@ export default function VariableList({ onInsert }) {
       <HelpText
         info="Click to insert placeholders in the email template. 
       These will be replaced with real values when the email is sent. 
-      Double click inserts it to editor."
+      Single click copies the variable name. Double click inserts it to editor."
       />
       <div
         style={{
@@ -47,7 +50,7 @@ export default function VariableList({ onInsert }) {
         {variables.map((v) => (
           <Button
             key={v.key}
-            onClick={handleCopy}
+            onClick={() => handleCopy(v.key)}
             onDoubleClick={() => onInsert(v.key)}
             title={v.description}
             style={{
@@ -67,6 +70,7 @@ export default function VariableList({ onInsert }) {
           </Button>
         ))}
       </div>
+      {copied == true && <span style={{fontWeight: 'bold'}}>Copied!</span>}
     </div>
   );
 }
