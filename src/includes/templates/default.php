a4a55ffc07673b4bf834316323d6a29defe52e40
Shashi Gharti <shashi.gharti@gmail.com>
2025-08-14 22:27:41 +0545

    Fix email theme design

--- src/includes/templates/default.php
+++ src/includes/templates/default.php
@@ -58,42 +58,29 @@
 <table width="100%" bgcolor="#F0F0F0" role="presentation">
   <tr>
     <td align="center">
-      <table class="wrapper" width="560" style="max-width: 560px;" align="center">
-        <tr>
-          <td align="center" style="padding: 20px 0;">
-            <a href="https://reel-reel.com/">
-              <img src="https://reel-reel.com/wp-content/uploads/2021/01/Logo-R-R-on-Bottom-Small.png" width="100" alt="Reel to Reel Logo" />
-            </a>
-          </td>
-        </tr>
-      </table>
-
+      
       <!-- BANNER WITH CENTERED MOTTO -->
-      <table class="container" bgcolor="#FFFFFF" width="560" style="max-width: 560px; border-radius: 8px; overflow: hidden;" align="center">
+      <table class="container" bgcolor="#FFFFFF" width="660" style="max-width:660px; border-radius: 8px; overflow: hidden;" align="center">
         <tr>
-          <td>
-            <table width="100%" height="200" cellpadding="0" cellspacing="0" border="0" background="https://reel-reel.com/wp-content/uploads/2016/09/1-1200x480.jpg" style="background-image: url('https://reel-reel.com/wp-content/uploads/2016/09/1-1200x480.jpg'); background-size: cover; background-position: center;">
+          <td width="100%" height="200" cellpadding="0" cellspacing="0" border="0"
+              background="https://reel-reel.com/wp-content/uploads/2016/09/1-1200x480.jpg"
+              style="background-image: url('https://reel-reel.com/wp-content/uploads/2016/09/1-1200x480.jpg'); background-size: cover; background-position: center;">
+              
+            <table role="presentation" width="100%" height="200" cellpadding="0" cellspacing="0" border="0">
               <tr>
-                <td align="center" valign="middle" height="200" style="background-color: rgba(0,0,0,0.45);">
-                  <table cellpadding="0" cellspacing="0" border="0" width="100%">
-                    <tr>
-                      <td align="center" style="padding: 0 15px;">
-                        <span style="display: block; font-weight: 700; font-size: 18px; text-transform: uppercase; letter-spacing: 2px; text-shadow: 1px 1px 4px rgba(0, 0, 0, 0.7); color: #FFFFFF; font-style: italic; font-family: Georgia, serif;">
-                          "THE DIRECTORY OF EVERYTHING - REEL TO REEL"
-                        </span>
-                      </td>
-                    </tr>
-                  </table>
+                <td align="center" valign="middle" style="background-color: rgba(219, 204, 204, 0.4);">
+                  <a href="https://reel-reel.com/">
+                    <img src="https://reel-reel.com/wp-content/uploads/2021/01/Logo-R-R-on-Bottom-Small.png" width="100" alt="Reel to Reel Logo" />
+                  </a>
                 </td>
               </tr>
             </table>
           </td>
         </tr>
-
+        
         <!-- MAIN CONTENT -->
         <tr>
           <td style="padding: 25px 6.25%;">
-            <h2 style="margin-top: 20px; text-align: center">Welcome to Reel to Reel!</h2>
             {{email_body}}
           </td>
         </tr>
@@ -110,39 +97,18 @@
             </table>
           </td>
         </tr>
-
-        <!-- SEPARATOR -->
-        <tr>
-          <td style="padding: 0 6.25%;">
-            <hr />
-          </td>
-        </tr>
-
-        <!-- SUPPORT INFO -->
-        <tr>
-          <td align="center" style="padding: 25px 6.25% 20px; font-size: 16px;">
-            Have questions? <a href="mailto:openreel@reel-reel.com" style="color: #7B1F1F;">Email us</a>
-          </td>
-        </tr>
       </table>
 
       <!-- FOOTER -->
-      <table class="wrapper" width="560" style="max-width: 560px;" align="center">
+      <table class="wrapper" width="660" style="max-width: 660px;" align="center">
         <tr>
-          <td align="center" style="padding: 25px 6.25%; color: #7B1F1F; font-size: 13px;">
-            <table width="256" align="center" style="margin-bottom: 10px;">
+          <td align="center" style="color: #7B1F1F; font-size: 13px;">
+            <table class="wrapper" width="660" style="max-width: 660px;" align="center">
               <tr>
-                <td style="padding: 0 10px;">
-                  <a href="https://facebook.com/reel2reel"><img src="https://raw.githubusercontent.com/konsav/email-templates/master/images/social-icons/facebook.png" width="44" alt="Facebook" /></a>
-                </td>
-                <td style="padding: 0 10px;">
-                  <a href="https://twitter.com/reel2reel"><img src="https://raw.githubusercontent.com/konsav/email-templates/master/images/social-icons/twitter.png" width="44" alt="Twitter" /></a>
-                </td>
-                <td style="padding: 0 10px;">
-                  <a href="https://instagram.com/reel2reel"><img src="https://raw.githubusercontent.com/konsav/email-templates/master/images/social-icons/instagram.png" width="44" alt="Instagram" /></a>
-                </td>
-                <td style="padding: 0 10px;">
-                  <a href="https://youtube.com/reel2reel"><img src="https://raw.githubusercontent.com/konsav/email-templates/master/images/social-icons/youtube.png" width="44" alt="YouTube" /></a>
+                <td align="center" style="padding: 20px 0;">
+                  <a href="https://reel-reel.com/">
+                    <img src="https://reel-reel.com/wp-content/uploads/2021/01/Logo-R-R-on-Bottom-Small.png" width="100" alt="Reel to Reel Logo" />
+                  </a>
                 </td>
               </tr>
             </table>
